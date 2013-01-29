1d3fb14510786b6481a9eb21e37382c4a0f5d89f
André Pacheco Neves <p4checo@gmail.com>
2013-01-29 17:01:52 +0000

    Added source code and unit test; updated README

---
+++ src/TestTripleBuffer.cpp
@@ -0,0 +1,85 @@
+//============================================================================
+// Name        : TestTripleBuffer.cpp
+// Author      : André Pacheco Neves
+// Version     : 1.0 (27/01/13)
+// Copyright   : Copyright (c) 2013, André Pacheco Neves
+//				 All rights reserved.
+//
+//				 Redistribution and use in source and binary forms, with or without
+//				 modification, are permitted provided that the following conditions are met:
+//					* Redistributions of source code must retain the above copyright
+//					  notice, this list of conditions and the following disclaimer.
+//					* Redistributions in binary form must reproduce the above copyright
+//					  notice, this list of conditions and the following disclaimer in the
+//					  documentation and/or other materials provided with the distribution.
+//					* Neither the name of the <organization> nor the
+//					  names of its contributors may be used to endorse or promote products
+//					  derived from this software without specific prior written permission.
+//
+//				 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//				 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//				 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//				 DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//				 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//				 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//				 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//				 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//				 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//				 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Description : TripleBuffer test class
+//============================================================================
+
+#include <assert.h>
+
+#include "TripleBuffer.hxx"
+template class TripleBuffer<int>; // explicit instantiation
+
+using namespace std;
+
+int main() {
+
+	TripleBuffer<int> buffer = TripleBuffer<int>(0);
+
+	/* Test 1 */
+
+	buffer.write(3);
+	buffer.flipWriter();
+
+	buffer.newSnap();
+	assert(buffer.snap() == 3); // <
+
+	/* Test 2 */
+
+	buffer.write(4);
+	buffer.flipWriter();
+	buffer.write(5);
+	buffer.flipWriter();
+	buffer.write(6);
+	buffer.flipWriter();
+
+	buffer.newSnap();
+
+	buffer.write(7);
+	buffer.flipWriter();
+	buffer.write(8);
+	buffer.flipWriter();
+	assert(buffer.snap() == 6); // <
+
+	/* Test 3 */
+
+	buffer.write(7);
+	buffer.flipWriter();
+
+	buffer.write(8);
+	buffer.flipWriter();
+
+	buffer.newSnap();
+	assert(buffer.snap() == 8); // <
+
+	buffer.newSnap();
+	assert(buffer.snap() == 8); // <
+
+	return 1;
+}
+
---
+++ src/TripleBuffer.hxx
@@ -0,0 +1,164 @@
+//============================================================================
+// Name        : TripleBuffer.hxx
+// Author      : André Pacheco Neves
+// Version     : 1.0 (27/01/13)
+// Copyright   : Copyright (c) 2013, André Pacheco Neves
+//				 All rights reserved.
+//
+//				 Redistribution and use in source and binary forms, with or without
+//				 modification, are permitted provided that the following conditions are met:
+//					* Redistributions of source code must retain the above copyright
+//					  notice, this list of conditions and the following disclaimer.
+//					* Redistributions in binary form must reproduce the above copyright
+//					  notice, this list of conditions and the following disclaimer in the
+//					  documentation and/or other materials provided with the distribution.
+//					* Neither the name of the <organization> nor the
+//					  names of its contributors may be used to endorse or promote products
+//					  derived from this software without specific prior written permission.
+//
+//				 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//				 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//				 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//				 DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//				 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//				 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//				 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//				 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//				 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//				 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Description : Template class for a TripleBuffer as a concurrency mechanism
+// Credits     : http://remis-thoughts.blogspot.pt/2012/01/triple-buffering-as-concurrency_30.html
+//============================================================================
+
+#ifndef TRIPLEBUFFER_HPP_
+#define TRIPLEBUFFER_HPP_
+
+#include <iostream>
+#include <atomic>
+
+using namespace std;
+
+template <typename T>
+class TripleBuffer
+{
+
+public:
+
+	TripleBuffer<T>();
+	TripleBuffer<T>(const TripleBuffer<T>&);
+	TripleBuffer<T>(const T& init);
+
+	T snap() const; // get the current snap to read
+	void write(const T newTransform); // write a new value
+	void newSnap(); // swap to the latest value, if any
+	void flipWriter(); // flip writer positions dirty / clean
+
+	T readLast(); // wrapper to read a value (newSnap + snap)
+	void update(T newT); // wrapper to write a new value (write + flipWriter)
+
+private:
+
+	// 8 bit flags are (unused) (new write) (2x dirty) (2x clean) (2x snap)
+	mutable atomic_uint_fast8_t flags;
+
+	T buffer[3];
+};
+
+// include implementation in header since it is a template
+
+template <typename T>
+TripleBuffer<T>::TripleBuffer(){
+
+	T dummy = T();
+
+	buffer[0] = dummy;
+	buffer[1] = dummy;
+	buffer[2] = dummy;
+
+	// initially dirty = 0, clean = 1 and snap = 2
+	flags = 0x6;
+
+	/*
+	uint_fast8_t snap = (flags & 0x3);
+	uint_fast8_t dirty = (flags & 0x30) >> 4;
+	uint_fast8_t clean = (flags & 0xC) >> 2;
+	 */
+}
+
+template <typename T>
+TripleBuffer<T>::TripleBuffer(const T& init){
+
+	buffer[0] = init;
+	buffer[1] = init;
+	buffer[2] = init;
+
+	// initially dirty = 0, clean = 1 and snap = 2
+	flags = 0x6;
+}
+
+
+template <typename T>
+TripleBuffer<T>::TripleBuffer(const TripleBuffer& t){
+
+	buffer[0] = t.buffer[0];
+	buffer[1] = t.buffer[1];
+	buffer[2] = t.buffer[2];
+
+	flags.store(t.flags);
+}
+
+template <typename T>
+T TripleBuffer<T>::snap() const{
+
+	return buffer[flags & 0x3]; // read snap index
+}
+
+template <typename T>
+void TripleBuffer<T>::write(const T newT){
+
+	buffer[(flags & 0x30) >> 4] = newT; // write into dirty index
+}
+
+template <typename T>
+void TripleBuffer<T>::newSnap(){
+
+	uint_fast8_t flagsNow;
+	uint_fast8_t newFlags;
+	do {
+		flagsNow = flags;
+		if( (flagsNow & 0x40)==0 ) // nothing new, no need to swap
+			break;
+		newFlags = (flagsNow & 0x30) | ((flagsNow & 0x3) << 2) | ((flagsNow & 0xC) >> 2); // swap snap with clean
+	} while(!flags.compare_exchange_strong(flagsNow,
+			newFlags,
+			memory_order_release,
+			memory_order_acquire));
+}
+
+template <typename T>
+void TripleBuffer<T>::flipWriter(){
+
+	uint_fast8_t flagsNow;
+	uint_fast8_t newFlags;
+	do {
+		flagsNow = flags;
+		newFlags = 0x40 | ((flagsNow & 0xC) << 2) | ((flagsNow & 0x30) >> 2) | (flagsNow & 0x3); // swap dirty with clean
+	} while(!flags.compare_exchange_strong(flagsNow,
+			newFlags,
+			memory_order_release,
+			memory_order_acquire));
+}
+
+template <typename T>
+T TripleBuffer<T>::readLast(){
+    newSnap(); // get most recent value
+    return snap(); // return it
+}
+
+template <typename T>
+void TripleBuffer<T>::update(T newT){
+    write(newT); // write new value
+    flipWriter(); // change dirty/clean buffer positions for the next update
+}
+
+#endif /* TRIPLEBUFFER_HPP_ */
