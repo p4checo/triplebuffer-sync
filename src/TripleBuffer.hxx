4c3f9b8be0b2ca9af7b9427d618d9876f91363d5
André Pacheco Neves <p4checo@gmail.com>
2013-02-02 23:38:16 +0000

    added copy assignment constructor attempt at move-and-swap idiom failed, because swap() of atomic_uint_fast8_t is not defined

--- src/TripleBuffer.hxx
+++ src/TripleBuffer.hxx
@@ -30,8 +30,8 @@
 // Credits     : http://remis-thoughts.blogspot.pt/2012/01/triple-buffering-as-concurrency_30.html
 //============================================================================
 
-#ifndef TRIPLEBUFFER_HPP_
-#define TRIPLEBUFFER_HPP_
+#ifndef TRIPLEBUFFER_HXX_
+#define TRIPLEBUFFER_HXX_
 
 #include <atomic>
 
@@ -40,30 +40,34 @@ using namespace std;
 template <typename T>
 class TripleBuffer
 {
-
+    
 public:
-
+    
 	TripleBuffer<T>();
 	TripleBuffer<T>(const TripleBuffer<T>&);
 	TripleBuffer<T>(const T& init);
-
+    TripleBuffer<T>& operator=(const TripleBuffer<T>&);
+    
+    //TripleBuffer<T>& operator=(TripleBuffer<T>); // copy-and-swap
+    //void swap(TripleBuffer<T>& t);
+    
 	T snap() const; // get the current snap to read
 	void write(const T newTransform); // write a new value
 	void newSnap(); // swap to the latest value, if any
 	void flipWriter(); // flip writer positions dirty / clean
-
+    
 	T readLast(); // wrapper to read the last available element (newSnap + snap)
 	void update(T newT); // wrapper to update with a new element (write + flipWriter)
-
+    
 private:
-
+    
 	// 8 bit flags are (unused) (new write) (2x dirty) (2x clean) (2x snap)
 	// newWrite   = (flags & 0x40)
 	// dirtyIndex = (flags & 0x30) >> 4
 	// cleanIndex = (flags & 0xC) >> 2
 	// snapIndex  = (flags & 0x3)
 	mutable atomic_uint_fast8_t flags;
-
+    
 	T buffer[3];
 };
 
@@ -71,51 +75,85 @@ private:
 
 template <typename T>
 TripleBuffer<T>::TripleBuffer(){
-
+    
 	T dummy = T();
-
+    
 	buffer[0] = dummy;
 	buffer[1] = dummy;
 	buffer[2] = dummy;
-
+    
 	flags = 0x6; // initially dirty = 0, clean = 1 and snap = 2
 }
 
 template <typename T>
 TripleBuffer<T>::TripleBuffer(const T& init){
-
+    
 	buffer[0] = init;
 	buffer[1] = init;
 	buffer[2] = init;
-
+    
 	flags = 0x6; // initially dirty = 0, clean = 1 and snap = 2
 }
 
 template <typename T>
 TripleBuffer<T>::TripleBuffer(const TripleBuffer& t){
-
+    
 	buffer[0] = t.buffer[0];
 	buffer[1] = t.buffer[1];
 	buffer[2] = t.buffer[2];
-
+    
 	flags.store(t.flags);
 }
 
 template <typename T>
-T TripleBuffer<T>::snap() const{
+TripleBuffer<T>& TripleBuffer<T>::operator=(const TripleBuffer<T>& t){
+    
+    // check for self-assignment
+    if (this != &t){
+        
+        // do the copy
+        buffer[0] = t.buffer[0];
+        buffer[1] = t.buffer[1];
+        buffer[2] = t.buffer[2];
+        flags = t.flags.load();
+    }
+    // return the existing object
+    return *this;
+}
 
+/*
+template <typename T>
+TripleBuffer<T>& TripleBuffer<T>::operator=(TripleBuffer<T> other){
+    
+    other.swap(*this);
+    
+    return *this;
+}
+
+template <typename T>
+void TripleBuffer<T>::swap(TripleBuffer<T>& t)
+{
+    
+    std::swap(this->flags, t.flags); // not yet defined
+    std::swap(this->buffer, t.buffer);
+}
+*/
+
+template <typename T>
+T TripleBuffer<T>::snap() const{
+    
 	return buffer[flags & 0x3]; // read snap index
 }
 
 template <typename T>
 void TripleBuffer<T>::write(const T newT){
-
+    
 	buffer[(flags & 0x30) >> 4] = newT; // write into dirty index
 }
 
 template <typename T>
 void TripleBuffer<T>::newSnap(){
-
+    
 	uint_fast8_t flagsNow;
 	uint_fast8_t newFlags;
 	do {
@@ -124,23 +162,23 @@ void TripleBuffer<T>::newSnap(){
 			break;
 		newFlags = (flagsNow & 0x30) | ((flagsNow & 0x3) << 2) | ((flagsNow & 0xC) >> 2); // swap snap with clean
 	} while(!flags.compare_exchange_strong(flagsNow,
-			newFlags,
-			memory_order_release,
-			memory_order_acquire));
+                                           newFlags,
+                                           memory_order_release,
+                                           memory_order_acquire));
 }
 
 template <typename T>
 void TripleBuffer<T>::flipWriter(){
-
+    
 	uint_fast8_t flagsNow;
 	uint_fast8_t newFlags;
 	do {
 		flagsNow = flags;
 		newFlags = 0x40 | ((flagsNow & 0xC) << 2) | ((flagsNow & 0x30) >> 2) | (flagsNow & 0x3); // set modified flag and swap clean with dirty
 	} while(!flags.compare_exchange_strong(flagsNow,
-			newFlags,
-			memory_order_release,
-			memory_order_acquire));
+                                           newFlags,
+                                           memory_order_release,
+                                           memory_order_acquire));
 }
 
 template <typename T>
@@ -155,4 +193,4 @@ void TripleBuffer<T>::update(T newT){
     flipWriter(); // change dirty/clean buffer positions for the next update
 }
 
-#endif /* TRIPLEBUFFER_HPP_ */
+#endif /* TRIPLEBUFFER_HXX_ */
